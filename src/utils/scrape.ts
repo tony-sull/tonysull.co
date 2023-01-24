9c567f81b04af2c1861edea96a00d91855b4f737
Tony Sullivan <tony.f.sullivan@outlook.com>
2023-01-23 22:04:47 -0600

    caching scrape URLs

--- src/utils/scrape.ts
+++ src/utils/scrape.ts
@@ -24,7 +24,10 @@ const scraper = metascraper([
   title(),
 ])
 
-async function fetchWithTimeout(resource: RequestInfo | URL, options: { timeout?: number } & RequestInit = {}) {
+async function fetchWithTimeout(
+  resource: RequestInfo | URL,
+  options: { timeout?: number } & RequestInit = {}
+) {
   const { timeout = 5000 } = options
 
   const controller = new AbortController()
@@ -32,15 +35,31 @@ async function fetchWithTimeout(resource: RequestInfo | URL, options: { timeout?
     controller.abort()
   }, timeout)
 
-  const response = await fetch(resource, { ...options, signal: controller.signal })
+  const response = await fetch(resource, {
+    ...options,
+    signal: controller.signal,
+  })
 
   clearTimeout(id)
 
   return response
 }
 
+const scrapeCache = new Map<string, Promise<string>>()
+
 export async function scrape(url: URL): Promise<Metadata> {
-  console.log('scraping', url.toString())
-  const content = await fetchWithTimeout(url).then(res => res.text())
+  if (!scrapeCache.has(url.toString())) {
+    scrapeCache.set(
+      url.toString(),
+      fetchWithTimeout(url, {
+        headers: {
+          'user-agent':
+            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+        },
+      }).then(res => res.text())
+    )
+  }
+
+  const content = await scrapeCache.get(url.toString())
   return await scraper({ url: url.toString(), html: content })
 }
