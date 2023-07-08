58c860b20ae1d298b1dda9345fa9266b08e4cd32
Tony Sullivan <15836226+tony-sull@users.noreply.github.com>
2023-07-08 08:18:49 -0500

    fixes for marked deprecation warnings

--- src/utils/markdown.ts
+++ src/utils/markdown.ts
@@ -1,7 +1,10 @@
 import { marked } from 'marked'
+import { gfmHeadingId } from 'marked-gfm-heading-id'
 import sanitizeHtml from 'sanitize-html'
 
+marked.use(gfmHeadingId())
+
 export function mdToHtml(markdown: string) {
-  const html = marked.parse(markdown)
+  const html = marked.parse(markdown, { mangle: false })
   return sanitizeHtml(html)
 }
