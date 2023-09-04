62184f27a867bd8343f89a9dc7bab52408c9e87e
Tony Sullivan <15836226+tony-sull@users.noreply.github.com>
2023-09-04 07:35:53 -0500

    updating deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,24 +4,21 @@ import webfinger from 'astro-webfinger'
 /* https://docs.netlify.com/configure-builds/environment-variables/#read-only-variables */
 import mdx from '@astrojs/mdx'
 const NETLIFY_PREVIEW_SITE =
-  process.env.CONTEXT !== 'production' && process.env.DEPLOY_PRIME_URL
+    process.env.CONTEXT !== 'production' && process.env.DEPLOY_PRIME_URL
 
 // https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
-    assets: true,
-  },
-  image: {
-    service: sharpImageService(),
-  },
-  site: NETLIFY_PREVIEW_SITE || 'https://tonysull.co',
-  integrations: [
-    mdx(),
-    webfinger({
-      instance: 'indieweb.social',
-      username: 'tonysull',
-    }),
-  ],
+    image: {
+        service: sharpImageService(),
+    },
+    site: NETLIFY_PREVIEW_SITE || 'https://tonysull.co',
+    integrations: [
+        mdx(),
+        webfinger({
+            instance: 'indieweb.social',
+            username: 'tonysull',
+        }),
+    ],
 })
