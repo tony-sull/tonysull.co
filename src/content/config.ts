22526ffe6fccffd0226d02f6471448f74000c2ab
Tony Sullivan <15836226+tony-sull@users.noreply.github.com>
2023-09-09 15:07:52 -0500

    fixing a few things

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,29 +1,29 @@
 import { defineCollection } from 'astro:content'
 import {
-    articleSchema,
-    bookmarkSchema,
-    noteSchema,
-    personSchema,
-    photoSchema,
+	articleSchema,
+	bookmarkSchema,
+	noteSchema,
+	personSchema,
+	photoSchema,
 } from '../types'
 
 export const collections = {
-    notes: defineCollection({
-        schema: noteSchema,
-    }),
-    articles: defineCollection({
-        schema: articleSchema,
-    }),
-    bookmarks: defineCollection({
-        schema: bookmarkSchema,
-    }),
-    personas: defineCollection({
-        schema: ({ image }) =>
-            personSchema({ image }).required({ nickname: true, logo: true }),
-    }),
-    photos: defineCollection({
-        schema: photoSchema,
-    }),
+	notes: defineCollection({
+		schema: noteSchema,
+	}),
+	articles: defineCollection({
+		schema: articleSchema,
+	}),
+	bookmarks: defineCollection({
+		schema: bookmarkSchema,
+	}),
+	personas: defineCollection({
+		schema: ({ image }) =>
+			personSchema({ image }).required({ nickname: true, logo: true }),
+	}),
+	photos: defineCollection({
+		schema: photoSchema,
+	}),
 }
 
-export type Collection = keyof typeof collections
+export type Collection = Exclude<keyof typeof collections, 'personas'>
