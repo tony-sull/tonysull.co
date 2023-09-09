6f7a29eccf2be8126a332a176b066205c3e733bb
Tony Sullivan <15836226+tony-sull@users.noreply.github.com>
2023-09-09 14:37:12 -0500

    routing for collections and basic article view

--- src/utils/scrape.ts
+++ src/utils/scrape.ts
@@ -7,62 +7,62 @@ import publisher from 'metascraper-publisher'
 import title from 'metascraper-title'
 
 const USER_AGENT =
-	'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
 
 export type Metadata = {
-	author?: string
-	description?: string
-	image?: string
-	logo?: string
-	publisher?: string
-	title: string
+    author?: string
+    description?: string
+    image?: string
+    logo?: string
+    publisher?: string
+    title: string
 }
 
 const scraper = metascraper([
-	author(),
-	description(),
-	image(),
-	logo(),
-	publisher(),
-	title(),
+    author(),
+    description(),
+    image(),
+    logo(),
+    publisher(),
+    title(),
 ])
 
 async function fetchWithTimeout(
-	resource: RequestInfo | URL,
-	options: { timeout?: number } & RequestInit = {},
+    resource: RequestInfo | URL,
+    options: { timeout?: number } & RequestInit = {},
 ) {
-	const { timeout = 5000 } = options
+    const { timeout = 5000 } = options
 
-	const controller = new AbortController()
-	const id = setTimeout(() => {
-		console.log('[fetchWithTimeout] aborted', resource.toString())
-		controller.abort()
-	}, timeout)
+    const controller = new AbortController()
+    const id = setTimeout(() => {
+        console.log('[fetchWithTimeout] aborted', resource.toString())
+        controller.abort()
+    }, timeout)
 
-	const response = await fetch(resource, {
-		...options,
-		signal: controller.signal,
-	})
+    const response = await fetch(resource, {
+        ...options,
+        signal: controller.signal,
+    })
 
-	clearTimeout(id)
+    clearTimeout(id)
 
-	return response
+    return response
 }
 
 const scrapeCache = new Map<string, Promise<string>>()
 
 export async function scrape(url: string): Promise<Metadata> {
-	if (!scrapeCache.has(url)) {
-		scrapeCache.set(
-			url,
-			fetchWithTimeout(url, {
-				headers: {
-					'user-agent': USER_AGENT,
-				},
-			}).then(res => res.text()),
-		)
-	}
+    if (!scrapeCache.has(url)) {
+        scrapeCache.set(
+            url,
+            fetch(url, {
+                headers: {
+                    'user-agent': USER_AGENT,
+                },
+            }).then(res => res.text()),
+        )
+    }
 
-	const content = await scrapeCache.get(url)
-	return await scraper({ url: url, html: content! })
+    const content = await scrapeCache.get(url)
+    return await scraper({ url: url, html: content! })
 }
